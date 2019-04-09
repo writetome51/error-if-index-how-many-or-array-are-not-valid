5a95104bb2088129eedf06f832525a73bc14ab2e
Steve Thompson <writetome51@gmail.com>
2019-04-09 17:20:12 -0600

    Updated dependencies. Upped version to 1.0.1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var error_if_index_not_valid_1 = require("error-if-index-not-valid");
-var errorIfNotArray_1 = require("basic-data-handling/errorIfNotArray");
-var errorIfNotIntegerZeroOrGreater_1 = require("basic-data-handling/errorIfNotIntegerZeroOrGreater");
+var errorIfNotArray_1 = require("error-if-not-array");
+var errorIfNotIntegerZeroOrGreater_1 = require("error-if-not-integer-zero-or-greater");
+
 // index can be negative or positive.
 function errorIfIndex_howMany_orArray_areNotValid(index, howMany, array) {
     errorIfNotArray_1.errorIfNotArray(array);
